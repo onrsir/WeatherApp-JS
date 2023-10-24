f7f7883c4ed40ea58cd2ee5e4953d60410dd5604
Onur Sır <115572997+onrsir@users.noreply.github.com>
2023-10-24 13:12:24 +0300

    Add files via upload

---
+++ weather-app/structure/main.js
@@ -0,0 +1,63 @@
+const apiKey = "26eebf5077c0c95796131d4cdfd8d4bc"
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
+
+const searchBox = document.querySelector(".search input")
+const searchBtn = document.querySelector(".search button")
+const weatherIcon = document.querySelector(".weather-icon")
+
+function gizle() {
+    document.querySelector(".error").style.display = "block"
+    document.querySelector(".weather").style.display = "none"
+}
+function göster() {
+    document.querySelector(".weather").style.display = "block"
+    document.querySelector(".error").style.display = "none"
+}
+
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + "&appid=" +apiKey);
+
+    if(response.status==404){
+        gizle()
+    }else {
+        var data = await response.json()
+        console.log(data)
+    
+        document.querySelector(".city").innerHTML = data.name
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C"
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%"
+        document.querySelector(".wind").innerHTML = data.wind.speed + "km/h"
+
+
+        if(data.weather[0].main == "Clouds"){
+            weatherIcon.src ="images/clouds.png"
+        } else if(data.weather[0].main == "Clear"){
+            weatherIcon.src ="images/clear.png"
+        }else if(data.weather[0].main == "Rain"){
+            weatherIcon.src ="images/rain.png"
+        }else if(data.weather[0].main == "Drizzle"){
+            weatherIcon.src ="images/drizzle.png"
+        }else if(data.weather[0].main == "Mist"){
+            weatherIcon.src ="images/mist.png"
+        }
+    
+        göster()
+    
+    }
+
+}
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value)
+})
+searchBox.addEventListener("keydown", (event) => {
+  if(event.key == "Enter"){
+    checkWeather(searchBox.value)
+  } else {
+    gizle()
+  }
+});
+
+
+
